67b53f819b29b3d01d34ba702a2d5ce82278f85c
Md Rahman <wrrobin@gmail.com>
2018-03-09 09:21:40 -0600

    Added csv file for plot

--- jenkins-scripts/perf_latency.sh
+++ jenkins-scripts/perf_latency.sh
@@ -73,11 +73,13 @@ scontrol show hostnames > hostfile
 
 if [ -f "$RESULT_DIR/lat_$BENCHMARK_$COMPILER" ]
 then
-    rm -rf $RESULT_DIR/lat_$BENCHMARK_$COMPILER
+    rm -rf $RESULT_DIR/lat_"$BENCHMARK"_"$COMPILER"
 fi
 
 oshrun -np 2 -ppn 1 -f hostfile $BENCH_HOME/$BENCHMARK > out_$BENCHMARK
-cat out_$BENCHMARK | grep "in bytes" -A24 | tail -n 24 > $RESULT_DIR/lat_"$BENCHMARK"_"$COMPILER"
+cat out_$BENCHMARK | grep "in bytes" -A24 | tail -n 24 > $WORKSPACE/lat_"$BENCHMARK"_"$COMPILER"
+
+cp lat_"$BENCHMARK"_"$COMPILER" $RESULT_DIR/
 
 EOF
 
