bddc2dd5267cb251832a26d72b016b0431c6164c
Md Rahman <md.rahman@intel.com>
2021-08-25 09:30:50 -0500

    enums

--- mpp/shmemx-def.h
+++ mpp/shmemx-def.h
@@ -22,6 +22,45 @@ typedef struct {
     uint64_t target;
 } shmemx_pcntr_t;
 
+
+#ifdef ENABLE_TOOLS
+typedef enum {
+    SHMEMX_T_VAR_TYPE_C,
+    SHMEMX_T_VAR_TYPE_P
+} shmemx_t_var_t;
+
+typedef enum {
+    SHMEMX_T_VAR_CAT_RMA,
+    SHMEMX_T_VAR_CAT_AMO,
+    SHMEMX_T_VAR_CAT_COLL
+} shmemx_t_var_cat;
+
+typedef enum {
+    SHMEMX_T_VAR_SUBCAT_PUT,
+    SHMEMX_T_VAR_SUBCAT_GET,
+    SHMEMX_T_VAR_SUBCAT_AND,
+    SHMEMX_T_VAR_SUBCAT_OR
+} shmemx_t_var_subcat;
+
+typedef enum {
+    SHMEMX_T_VAR_BIND_CTX,
+    SHMEMX_T_VAR_BIND_TEAM,
+    SHMEMX_T_VAR_BIND_PE
+} shmemx_t_var_bind;
+
+typedef enum {
+    SHMEMX_T_VAR_OP_ISSUED,
+    SHMEMX_T_VAR_OP_PENDING,
+    SHMEMX_T_VAR_OP_COMPLETE
+} shmemx_t_var_op;
+
+typedef enum {
+    SHMEMX_T_VAR_CLASS_COUNTER,
+    SHMEMX_T_VAR_CLASS_AGGREGATE,
+    SHMEMX_T_VAR_CLASS_PERCENT
+} shmemx_t_var_class;
+
+#endif /* SHMEM_T */
 #ifdef __cplusplus
 }
 #endif
